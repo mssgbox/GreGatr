e00a04e564fee6d1b69675b622730ae125f72f27
mssg box <message>
2025-04-16 16:19:06 +0200

    moved the old Aggregator integration test from Unit to Integration tests

--- GreGatr.Tests.Unit/AggregatorTests.cs
+++ GreGatr.Tests.Unit/AggregatorTests.cs
@@ -39,25 +39,6 @@ namespace GreGatr.Tests.Unit
             if (File.Exists(OutputFilePath)) File.Delete(OutputFilePath);
         }
 
-        [Test]
-        public async Task TestAggregateMoviesAsync()
-        {
-            var aggregator = new Aggregator();
-
-            await aggregator.AggregateMoviesAsync();
-
-            // Validate that the output file was written
-            Assert.That(File.Exists(Settings.OutputFileName), Is.True);
-
-            // Optionally, validate the content of the output file (here we assume a specific structure or summary)
-            SyndicationFeed outputFeed;
-            using (var reader = XmlReader.Create(Settings.OutputFileName))
-            {
-                outputFeed = SyndicationFeed.Load(reader);
-            }
-            var firstScore = outputFeed.Items.FirstOrDefault()?.ElementExtensions.First();
-            Assert.That(firstScore?.OuterName, Is.EqualTo(Settings.ResultNameRT));
-        }
 
         [Test]
         public void GenerateAndWriteFeed_ShouldWriteFeedToFile()
