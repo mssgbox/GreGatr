53c0f0e2b5e42168e0c89bfeb05601ad7ed75bdd
mssgbox <166498112+mssgbox@users.noreply.github.com>
2026-06-11 11:57:36 -0400

    Add System.Net.Http using directive to Fetcher.cs

--- Gregatr.Domain/Services/Fetcher.cs
+++ Gregatr.Domain/Services/Fetcher.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Net;
+using System.Net.Http;
 using System.ServiceModel.Syndication;
 using System.Text;
 using System.Threading.Tasks;
